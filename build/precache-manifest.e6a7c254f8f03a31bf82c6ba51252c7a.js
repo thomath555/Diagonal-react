1d57451f3fd163ea2dba871875084b09f37f1a04
thomas <thomath47@hotmail.com>
2019-06-15 03:14:01 +0530

    build added

---
+++ build/precache-manifest.e6a7c254f8f03a31bf82c6ba51252c7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb37f8510802825e3c858155baede45",
+    "url": "/Diagonal-react/index.html"
+  },
+  {
+    "revision": "df58deecd0c3c407aa3d",
+    "url": "/Diagonal-react/static/css/main.942bc8d1.chunk.css"
+  },
+  {
+    "revision": "08576179dd23d8ca59d1",
+    "url": "/Diagonal-react/static/js/2.d0c7602d.chunk.js"
+  },
+  {
+    "revision": "df58deecd0c3c407aa3d",
+    "url": "/Diagonal-react/static/js/main.73fe9208.chunk.js"
+  },
+  {
+    "revision": "2604ad9d32a54ab0fd8b",
+    "url": "/Diagonal-react/static/js/runtime~main.90aaeb89.js"
+  }
+]);
